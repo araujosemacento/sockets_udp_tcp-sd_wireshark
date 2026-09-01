658ea874c11ed82358e386c41aa44a468c97c1eb
G. Melo <araujosemacento@alu.ufc.br>
2026-09-01 13:53:26 -0300

    feat: Simular os servidores TCP e UDP desligados
    - Configurando conexões pra porta uma porta inativa
    - Tratamento de erro de rejeição de conexão no TCP
    - Adiciona timeout de 2 segundos com fechamento do socket no UDP

--- server.js
+++ server.js
@@ -17,13 +17,31 @@ http.createServer((req, res) => {
         const proto = url.searchParams.get('proto');
         const msg = url.searchParams.get('msg');
 
-        if (proto === 'tcp') {
-            const client = net.connect(3030, '127.0.0.1', () => client.write(msg));
-            client.on('data', data => { res.end(data); client.end(); });
-        } else {
-            const client = dgram.createSocket('udp4');
-            client.send(msg, 3333, '127.0.0.1');
-            client.on('message', data => { res.end(data); client.close(); });
+        switch (proto) {
+            case 'tcp': {
+                const client = net.connect(3030, '127.0.0.1', () => client.write(msg));
+                client.on('data', data => { res.end(data); client.end(); });
+                break;
+            }
+            case 'udp': {
+                const client = dgram.createSocket('udp4');
+                client.send(msg, 3333, '127.0.0.1');
+                client.on('message', data => { res.end(data); client.close(); });
+                break;
+            }
+            case 'tcp-null': {
+                const client = net.connect(9999, '127.0.0.1', () => client.write(msg));
+                client.on('data', data => { res.end(data); client.end(); });
+                client.on('error', () => { res.end('Falha na conexão. Servidor TCP inalcançavel.'); });
+                break;
+            }
+            case 'udp-null': {
+                const client = dgram.createSocket('udp4');
+                client.send(msg, 9999, '127.0.0.1');
+                const timeout = setTimeout(() => { res.end('Falha na conexão. Servidor UDP sem resposta.'); client.close(); }, 2000);
+                client.on('message', data => { clearTimeout(timeout); res.end(data); client.close(); });
+                break;
+            }
         }
     }
 }).listen(3000, () => console.log('Servidor Web rodando em http://localhost:3000'));
